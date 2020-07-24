6b2d229b93828f72ea3a8fef584098b33a5ddd33
Aprilian Nur <aprilian@rencanamu.id>
2020-07-25 00:41:33 +0700

    redesign snackbar, add position, and add in-out animation

--- app/src/main/java/com/aprealian/sample/MainActivity.kt
+++ app/src/main/java/com/aprealian/sample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.aprealian.sample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aprealian.wundersnackbar.core.WunderSnackBar
+import com.aprealian.wundersnackbar.style.Position
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun testSnackBar() {
-        val wunderSnackBar = WunderSnackBar().make(coordinatorLayout, "WiFi change detected; updating information...")
+        val wunderSnackBar = WunderSnackBar(this).make(coordinatorLayout, "WiFi change detected; updating information...", position = Position.BOTTOM)
         wunderSnackBar.show()
     }
 }
--- wundersnackbar/src/main/java/com/aprealian/wundersnackbar/core/WunderSnackBar.kt
+++ wundersnackbar/src/main/java/com/aprealian/wundersnackbar/core/WunderSnackBar.kt
@@ -1,55 +1,146 @@
 package com.aprealian.wundersnackbar.core
 
 import android.app.Activity
+import android.content.Context
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
-import androidx.coordinatorlayout.widget.CoordinatorLayout
+import android.view.ViewGroup
+import android.view.animation.TranslateAnimation
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import com.aprealian.wundersnackbar.R
 import com.aprealian.wundersnackbar.style.Margin
+import com.aprealian.wundersnackbar.style.Position
 import com.aprealian.wundersnackbar.style.Properties
+import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
-import java.lang.Exception
 
 
-class WunderSnackBar {
+class WunderSnackBar(val activity: Activity) {
 
     private var snackBar: Snackbar? = null
     private var snackBarView: View? = null
     private var margin: Margin? = null
+    private var position: Int? = Position.BOTTOM
 
     init {
         snackBarView = snackBar?.view
-        setMargin()
+        //setMargin()
     }
 
     fun make(view: View,
              text: CharSequence,
+             position: Int = Position.BOTTOM,
              duration: Int = Snackbar.LENGTH_SHORT
     ): WunderSnackBar {
-        //snackBar = Snackbar.make(view, text, duration);
-        snackBar = with(Snackbar.make(view, text, duration)) {
-            this
-        }
+
+        this.position = position
+
+
+        snackBar = Snackbar.make(view, text, duration)
+
+
+        //this code will resolve animation issue, thanks to: https://stackoverflow.com/questions/31746300/how-to-show-snackbar-at-top-of-the-screen/58183630#58183630
+        snackBar?.animationMode = BaseTransientBottomBar.ANIMATION_MODE_FADE
+
+        //background
+        //snackBar?.view?.background = ContextCompat.getDrawable(activity, R.drawable.snackbar_shape) // min API 16
+        snackBar?.view?.background = null //remove background
+
+        //custom layout
+        customLayout(position)
+
+        //set margin
+        setMargin()
+
         return this
     }
 
+    private fun customLayout(position: Int) {
+        // Get the Snackbar's layout view
+        val layout = snackBar?.view as (Snackbar.SnackbarLayout)? ?: return
+
+        // Hide the text
+        val textView = layout.findViewById<View>(R.id.snackbar_text) as TextView
+        textView.visibility = View.INVISIBLE
+
+        // Inflate our custom view
+        val mInflater = activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val snackView: View = mInflater.inflate(R.layout.my_snackbar, null)
+        val imageView: ImageView = snackView.findViewById<View>(R.id.image) as ImageView
+        imageView.setImageResource(android.R.drawable.ic_btn_speak_now)
+        val textViewTop = snackView.findViewById<View>(R.id.text) as TextView
+
+        //If the view is not covering the whole snackbar layout, add this line
+        layout.setPadding(0, 0, 0, 0)
+
+        // Add the view to the Snackbar's layout
+        val snackLayout: Snackbar.SnackbarLayout = snackBar?.view as Snackbar.SnackbarLayout
+        val params  = snackLayout.layoutParams as FrameLayout.LayoutParams
+        //params.height = 500
+        //params.width = 500
+
+        if (position == Position.TOP){
+            params.gravity = Gravity.TOP or Gravity.CENTER_HORIZONTAL
+        } else {
+            params.gravity = Gravity.BOTTOM or Gravity.CENTER_HORIZONTAL
+        }
+
+
+        // Add the view to the Snackbar's layout
+        layout.addView(snackView, 0, params)
+    }
+
     private fun setMargin(){
-        try {
-            val params = snackBarView?.layoutParams as (CoordinatorLayout.LayoutParams)
-            if (margin != null){
-                margin?.let {
-                    params.setMargins(
-                        params.leftMargin + it.left,
-                        params.topMargin + it.top,
-                        params.rightMargin + it.right,
-                        params.bottomMargin + it.bottom
-                    )
-                    snackBarView?.setLayoutParams(params)
-                }
-            }
-        } catch (e: Exception){ }
+
+        margin?.let {
+
+        }
+
+        val cardView = snackBar?.view?.findViewById<CardView>(R.id.cardView)
+        cardView?.apply {
+            val params = layoutParams as ViewGroup.MarginLayoutParams
+            //params.setMargins(10, 10, 10, 100)
+            cardView.requestLayout()
+        }
+
     }
 
     fun show(){
         snackBar?.show()
+
+        if (position == Position.TOP){
+            animationOut()
+        } else {
+            animationIn()
+        }
+    }
+
+    private fun animationIn() {
+        var slide = TranslateAnimation(0F, 0F, 0F, (- snackBar?.view?.height!!).toFloat())
+        slide.duration = 300
+        slide.fillAfter = true
+        snackBar?.view?.startAnimation(slide)
+        slide = TranslateAnimation(0F, 0F, 100F, 0F) // seems you have a problem with 3rd param
+
+        slide.duration = 1000
+        slide.fillAfter = true
+        snackBar?.view?.startAnimation(slide)
+    }
+
+    private fun animationOut() {
+        var slide = TranslateAnimation(0F, 0F, 0F, 0F)
+        slide.duration = 300
+        slide.fillAfter = true
+        snackBar?.view?.startAnimation(slide)
+        slide = TranslateAnimation(0F, 0F, -100F, 0F) // seems you have a problem with 3rd param
+
+        slide.duration = 1000
+        slide.fillAfter = true
+        snackBar?.view?.startAnimation(slide)
     }
 
     fun dismiss() {
@@ -58,7 +149,7 @@ class WunderSnackBar {
 
     companion object {
         internal fun instance(activity: Activity, props: Properties) =
-            WunderSnackBar().apply {
+            WunderSnackBar(activity).apply {
                 margin = props.margin
             }
     }
